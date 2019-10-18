39c1d6d48c94fa97b6d42602f3205f6511eec29e
Nat Welch <nat@natwelch.com>
2019-10-18 12:24:51 +0000

    Fix some types

--- lib/lib.go
+++ lib/lib.go
@@ -8,17 +8,15 @@ import (
 )
 
 // Report is a simple interface for types exported by ParseReport.
-type Report interface {
-	IsReport() bool
+type Report struct {
+	ExpectCT *ExpectCTReport
+	CSP      *CSPReport
+	ReportTo *[]ReportToReport
 }
 
 // ExpectCTReport is the struct for Expect-CT errors.
 type ExpectCTReport struct {
 	ExpectCTReport ExpectCTSubReport `json:"expect-ct-report"`
-}
-
-func (e ExpectCTReport) IsReport() bool {
-	return true
 }
 
 // ExpectCTSubReport is the internal datastructure of an ExpectCTReport.
@@ -85,13 +83,9 @@ type ReportToReport struct {
 	} `json:"body"`
 }
 
-func (r ReportToReport) IsReport() bool {
-	return true
-}
-
 // ParseReport takes a content-type header and a body json string and parses it
 // into valid Go structs.
-func ParseReport(ct, body string) ([]Report, error) {
+func ParseReport(ct, body string) (*Report, error) {
 	media, _, err := mime.ParseMediaType(ct)
 	if err != nil {
 		return nil, err
@@ -104,21 +98,21 @@ func ParseReport(ct, body string) ([]Report, error) {
 		if err != nil {
 			return nil, err
 		}
-		return data, nil
+		return &Report{ReportTo: &data}, nil
 	case "application/expect-ct-report+json":
 		var data ExpectCTReport
 		err := json.Unmarshal([]byte(body), &data)
 		if err != nil {
 			return nil, err
 		}
-		return []Report{data}, nil
+		return &Report{ExpectCT: &data}, nil
 	case "application/csp-report":
 		var data CSPReport
 		err := json.Unmarshal([]byte(body), &data)
 		if err != nil {
 			return nil, err
 		}
-		return data, nil
+		return &Report{CSP: &data}, nil
 	}
 
 	return nil, fmt.Errorf("\"%s\" is not a valid content-type", media)
