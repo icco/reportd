87b8694c78a6fb5f23ee17f9fc47115f35605a41
Nat Welch <nat@natwelch.com>
2021-04-03 03:17:08 +0000

    Revert "nullable"
    This reverts commit 414958a3363bce1bdfbc553be3bbd7edddad1b87.

--- lib/reports.go
+++ lib/reports.go
@@ -13,9 +13,9 @@ import (
 
 // Report is a simple interface for types exported by ParseReport.
 type Report struct {
-	ExpectCT *ExpectCTReport   `bigquery:",nullable"`
-	CSP      *CSPReport        `bigquery:",nullable"`
-	ReportTo []*ReportToReport `bigquery:",nullable"`
+	ExpectCT *ExpectCTReport
+	CSP      *CSPReport
+	ReportTo []*ReportToReport
 }
 
 // ExpectCTReport is the struct for Expect-CT errors.
